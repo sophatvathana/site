f203e93a32f1a384dece367b874c230c05a7d12b
sima.zhang1990@gmail.com <zaifei.zwx@alibaba-inc.com>
2018-03-06 11:25:13 +0800

    update f2 api docs

--- site-config.js
+++ site-config.js
@@ -7,7 +7,7 @@ var pkg = require('./package.json');
 module.exports = {
     url: {
         dataSet: 'https://gw.alipayobjects.com/os/antv/assets/data-set/0.8.5/data-set.min.js',
-        f2: 'https://gw.alipayobjects.com/os/antv/assets/f2/3.0.3/f2.js',
+        f2: 'https://unpkg.com/@antv/f2@3.1.1-beta.5/build/f2.js',
         g2: 'https://gw.alipayobjects.com/os/antv/assets/g2/3.0.5-beta.4/g2.min.js',
         g6: 'https://gw.alipayobjects.com/os/antv/assets/g6/1.2.5/g6.min.js',
         'g6-plugins': 'https://gw.alipayobjects.com/os/antv/assets/g6-plugins/1.0.1/g6-plugins.min.js',
